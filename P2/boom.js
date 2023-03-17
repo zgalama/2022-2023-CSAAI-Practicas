6c1a6d12f886b482f25ca5acc3b14e9af2388079
zgalama <zg.alama.2020@alumnos.urjc.es>
2023-03-17 17:28:49 +0100

    cambios

--- P2/boom.js
+++ P2/boom.js
@@ -63,10 +63,6 @@ function digito(value)
 }
 
 for (let boton of gui.botones) {
-
-  //-- Establecer la funcion de llamada del boton i
-  //-- El parámetro ev.target contiene el boton
-  //-- que ha recibido el click
   boton.onclick = (ev) => {
     digito(ev.target.value)
     crono.start()
